29f414fa25ec66585d122ebdbb87f144ae3b086b
Berhan <bcgundogdu@gmail.com>
2017-02-01 21:27:29 +0300

    heroku setup

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -79,7 +79,9 @@ public class App
     static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
+	    int port = Integer.parseInt(processBuilder.environment().get("PORT"));
+	    System.out.println(port);
+            return port;
         }
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
